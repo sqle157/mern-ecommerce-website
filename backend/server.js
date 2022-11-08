c5373b8bc85f574ed837f1c5b4a881f3eb0917ea
sqle157 <sql@Sangs-MacBook-Pro.local>
2022-11-07 23:14:12 -0700

    Minor change to server.js

--- backend/server.js
+++ backend/server.js
@@ -23,11 +23,17 @@ app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/order', require('./routes/orderRoutes'));
 
 // Serve frontend
-// Set build folder as static
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+if (process.env.NODE_ENV === 'production') {
+	// Set build folder as static
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('*', (req, res) =>
-	res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-);
+	app.get('*', (req, res) =>
+		res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+	);
+} else {
+	app.get('/', (req, res) => {
+		res.status(200).json({ message: 'Welcome to the Ecommerce App API' });
+	});
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
